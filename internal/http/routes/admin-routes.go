e5ba7626b3e191fd9c72693c6b8f204f2d7bba54
bnema <brice@briceamen.com>
2023-09-05 09:01:30 +0200

    Create install route and template, auto redirect if config file is not present Enhanced logging: Redirected stdout to custom zerolog logger, removed redundant EnableLogger function, and captured all fmt messages.

--- internal/http/routes/admin-routes.go
+++ internal/http/routes/admin-routes.go
@@ -11,9 +11,14 @@ import (
 )
 
 func AdminRoute(c echo.Context) error {
+	// Check if it's the first launch of the app
+	if utils.IsConfigFilePresent() {
+		fmt.Println("Fresh installation detected, redirecting to /admin/install")
+		return c.Redirect(http.StatusFound, "/admin/install")
+	}
+
 	// Retrieve the current language from the context
 	lang := c.Get(middlewares.LangKey).(string)
-	fmt.Println(lang)
 
 	staticData, err := utils.PopulateDataFromYAML(lang)
 	if err != nil {
@@ -21,6 +26,36 @@ func AdminRoute(c echo.Context) error {
 	}
 
 	renderer, err := utils.GetRenderer("index.gohtml", ui.PublicFS, utils.NewLogger())
+	if err != nil {
+		return err
+	}
+
+	html, err := renderer.Render(staticData.CurrentLang)
+	if err != nil {
+		// Return the error into NewLogger() to log it
+		utils.NewLogger().Error()
+		return err
+	}
+
+	return c.HTML(http.StatusOK, html)
+}
+
+func InstallRoute(c echo.Context) error {
+	// Check if it's the first launch of the app
+	if !utils.IsConfigFilePresent() {
+		fmt.Println("Config file already present, redirecting to /admin")
+		return c.Redirect(http.StatusFound, "/admin")
+	}
+
+	// Retrieve the current language from the context
+	lang := c.Get(middlewares.LangKey).(string)
+
+	staticData, err := utils.PopulateDataFromYAML(lang)
+	if err != nil {
+		return err
+	}
+
+	renderer, err := utils.GetRenderer("install.gohtml", ui.PublicFS, utils.NewLogger())
 	if err != nil {
 		return err
 	}
