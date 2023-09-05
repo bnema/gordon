adf1635ad574db40c35c2fdd090849fca064ec8f
bnema <hi@bnema.dev>
2023-09-05 06:55:26 +0200

    Enable ColorSchemeDetection middleware

--- internal/http/routes/router.go
+++ internal/http/routes/router.go
@@ -11,9 +11,10 @@ func ConfigureRouter(e *echo.Echo, app *app.App) *echo.Echo {
 	// The logger is already set in main.go, so we don't need to set it again here.
 
 	// Register middlewares
-	e.Use(middlewares.NewRequestLoggerMiddleware(app.HTTPLogger.Logger))
-	e.Use(middlewares.LanguageDetectionMiddleware)
-	// e.Use(middlewares.SecureMiddleware()) // Uncomment this line to enable the secure middleware
+	e.Use(middlewares.NewRequestLogger(app.HTTPLogger.Logger))
+	e.Use(middlewares.LanguageDetection)
+	e.Use(middlewares.ColorSchemeDetection)
+	//e.Use(middlewares.SecureMiddleware()) // Uncomment this line to enable the secure middleware
 	// Register routes
 	e = bindStaticAdminUI(e)
 
