212dd8d915865a991c7d15258f458a4d45e8345c
bnema <brice@briceamen.com>
2023-09-06 18:41:52 +0200

    feat: Added a `Config` struct and `GetConfig` function in config-provider.go
    - Introduced a `Config` struct for better configuration handling.
    - Created a `GetConfig` function to retrieve the configuration.
    
    fix: Improved error handling and removed debug prints in htmx-installer.go
    
    - Enhanced error handling in the Traefik installer.
    - Removed unnecessary debug print statements.
    
    refactor: Updated package dependencies and templates
    
    - Updated package-lock.json and package.json for tailwindcss.
    - Made template improvements in traefik-compose.goyml and traefik.goyml.
    
    misc:
    
    - Included the @catppuccin/tailwindcss plugin in the tailwind configuration.

--- pkg/scripts/install-traefik.go
+++ pkg/scripts/install-traefik.go
@@ -1,15 +1,34 @@
 package scripts
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
+	root "gogs.bnema.dev/gordon-echo"
+	"gogs.bnema.dev/gordon-echo/internal/app"
 	"gogs.bnema.dev/gordon-echo/pkg/templating"
 	"gogs.bnema.dev/gordon-echo/pkg/utils"
 )
 
 func CreateYAMLFiles(topDomain, adminEmail string, logger *utils.Logger) error {
+	path := app.GetBuildDir() + "/"
+	modelFS := root.ModelFS
+	if modelFS == nil {
+		return errors.New("modelFS is nil")
+	}
+
+	// Check if directory traefik exists, if not create it
+	if _, err := os.Stat(path + "traefik"); os.IsNotExist(err) {
+		err = os.Mkdir(path+"traefik", 0755)
+		if err != nil {
+			return err
+		}
+	}
+
 	// Setup the TXT renderer for traefik-compose.goyml
-	rendererCompose, err := templating.GetTXTRenderer("traefik-compose.goyml", nil, logger)
+	rendererCompose, err := templating.GetTXTRenderer("traefik-compose.goyml", modelFS, logger)
+	fmt.Println("rendererCompose=", rendererCompose)
 	if err != nil {
 		return err
 	}
@@ -18,18 +37,18 @@ func CreateYAMLFiles(topDomain, adminEmail string, logger *utils.Logger) error {
 	composeContent, err := rendererCompose.TXTRender(map[string]string{
 		"topdomain": topDomain,
 	})
+	fmt.Println("composeContent=", composeContent)
 	if err != nil {
 		return err
 	}
-
-	// Save the rendered content to a file
-	err = os.WriteFile("traefik-compose.yml", []byte(composeContent), 0644)
+	// Save the rendered content to a file at the env path
+	err = os.WriteFile(path+"docker-compose.yml", []byte(composeContent), 0644)
 	if err != nil {
 		return err
 	}
 
 	// Setup the TXT renderer for traefik.goyml
-	rendererTraefik, err := templating.GetTXTRenderer("traefik.goyml", nil, logger)
+	rendererTraefik, err := templating.GetTXTRenderer("traefik.goyml", modelFS, logger)
 	if err != nil {
 		return err
 	}
@@ -42,11 +61,12 @@ func CreateYAMLFiles(topDomain, adminEmail string, logger *utils.Logger) error {
 		return err
 	}
 
-	// Save the rendered content to a file
-	err = os.WriteFile("traefik/traefik.yml", []byte(traefikContent), 0644)
+	// Save the rendered content to a file at the env path
+	err = os.WriteFile(path+"traefik/traefik.yml", []byte(traefikContent), 0644)
 	if err != nil {
 		return err
 	}
 
 	return nil
+
 }
