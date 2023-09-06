d3164d2d32c1a37b4dd48abfbe70bf8e0b2ade71
bnema <brice@briceamen.com>
2023-09-06 20:23:06 +0200

    WIP

--- internal/http/handlers/htmx-installer.go
+++ internal/http/handlers/htmx-installer.go
@@ -1,14 +1,20 @@
 package handlers
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"gogs.bnema.dev/gordon-echo/internal/app"
 	"gogs.bnema.dev/gordon-echo/pkg/scripts"
 )
 
+var eventsChan = make(chan string)
+
 // TraefikInstallerHandler handles the installation of Traefik
 func TraefikInstallerHandler(c echo.Context, a *app.App) error {
 	logger := a.AppLogger
+	eventsChan <- "Starting Traefik installation..."
 	// Retrieve the logger from the context
 	// Reject POST requests if they are not allowed with the RejectPOSTPolicy function
 	if err := RejectPOSTPolicy(c); err != nil {
@@ -36,9 +42,34 @@ func TraefikInstallerHandler(c echo.Context, a *app.App) error {
 		logger.Error().Msg("Failed to install Traefik")
 		return err
 	} else {
-		logger.Info().Msg("Traefik installed successfully")
+		eventsChan <- "Traefik installation completed!"
 		return c.String(200, "Traefik installed successfully")
 	}
 }
 
-// use the handler at the route /admin/install/traefik
+func TraefikInstallerSSEHandler(c echo.Context, a *app.App) error {
+	w := c.Response().Writer
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		return c.String(http.StatusInternalServerError, "Streaming unsupported!")
+	}
+
+	c.Response().Header().Set("Content-Type", "text/event-stream")
+	c.Response().Header().Set("Cache-Control", "no-cache")
+	c.Response().Header().Set("Connection", "keep-alive")
+
+	for {
+		// Listen for a new message to be sent to the client
+		msg, open := <-eventsChan
+		if !open {
+			fmt.Println("Connection closed")
+			break
+		}
+		_, err := fmt.Fprintf(w, "data: %s\n\n", msg)
+		if err != nil {
+			return err
+		}
+		flusher.Flush()
+	}
+	return nil
+}
