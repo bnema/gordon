8151903479fcc444669587532292894717a4fe85
bnema <hi@bnema.dev>
2023-09-12 19:30:59 +0200

    New structure and embedding ok

---
+++ internal/gotemplate/embed.go
@@ -0,0 +1,13 @@
+package gotemplate
+
+import (
+	"embed"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Embedding templates directories
+
+//go:embed models/*
+var template embed.FS
+var TemplateFS = echo.MustSubFS(template, "models")
