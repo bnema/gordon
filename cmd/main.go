654acb67ca3a378231b722b4d43b21d4c1b26708
bnema <hi@bnema.dev>
2023-09-13 18:07:51 +0200

    Integration Echo: ok

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"log/slog"
+
 	"github.com/bnema/gordon/internal/app"
+	"github.com/bnema/gordon/internal/httpserve"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	a := app.NewApp()
-	a.Start()
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+	e = httpserve.RegisterRoutes(e, a)
+	slog.Info("Starting server", "port", a.HttpPort)
+	slog.Error("Server error", e.Start(fmt.Sprintf(":%d", a.HttpPort)))
 }
