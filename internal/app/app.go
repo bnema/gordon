a4391eec12fb829d9ae8e66e9a81f1199ed72dfd
bnema <hi@bnema.dev>
2023-09-15 20:55:42 +0200

    Add handlers for oauth, add session middleware, define routes for login/logout

--- internal/app/app.go
+++ internal/app/app.go
@@ -18,41 +18,44 @@ const (
 	AdminPath  = "/admin"
 	DockerSock = "/var/run/docker.sock"
 	// If enabled replace all docker commands by podman
-	PodmanEnable = false
-	PodmanSock   = "/run/user/1000/podman/podman.sock"
+	PodmanEnable     = false
+	PodmanSock       = "/run/user/1000/podman/podman.sock"
+	OauthCallbackURL = "http://localhost:1323/oauth/callback"
 )
 
 type App struct {
-	TemplateFS      fs.FS
-	PublicFS        fs.FS
-	BuildVersion    string
-	BuildDir        string
-	DBDir           string
-	DBFilename      string
-	DBPath          string
-	InitialChecksum string
-	DB              *sql.DB
-	TopDomain       string
-	SubDomain       string
-	AdminPath       string
-	DockerSock      string
-	PodmanEnable    bool
-	PodmanSock      string
-	HttpPort        int16
+	TemplateFS       fs.FS
+	PublicFS         fs.FS
+	BuildVersion     string
+	BuildDir         string
+	DBDir            string
+	DBFilename       string
+	DBPath           string
+	InitialChecksum  string
+	DB               *sql.DB
+	TopDomain        string
+	SubDomain        string
+	AdminPath        string
+	DockerSock       string
+	PodmanEnable     bool
+	PodmanSock       string
+	OauthCallbackURL string
+	HttpPort         int16
 }
 
 func NewApp() *App {
 	return &App{
-		TemplateFS:   gotemplate.TemplateFS,
-		PublicFS:     webui.PublicFS,
-		BuildVersion: BuildVersion,
-		BuildDir:     BuildDir,
-		DBDir:        DBDir,
-		DBFilename:   DBFilename,
-		TopDomain:    TopDomain,
-		SubDomain:    SubDomain,
-		AdminPath:    AdminPath,
-		HttpPort:     HttpPort,
+		TemplateFS:       gotemplate.TemplateFS,
+		PublicFS:         webui.PublicFS,
+		BuildVersion:     BuildVersion,
+		BuildDir:         BuildDir,
+		DBDir:            DBDir,
+		DBFilename:       DBFilename,
+		TopDomain:        TopDomain,
+		SubDomain:        SubDomain,
+		AdminPath:        AdminPath,
+		OauthCallbackURL: OauthCallbackURL,
+		HttpPort:         HttpPort,
 	}
 }
 
