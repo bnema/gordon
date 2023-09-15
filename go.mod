ae815f027d7a8b8e0339025635438ed4a2c29dd9
Brice <hi@bnema.dev>
2023-09-15 07:39:13 +0200

    rewrite sqlite.go logic flow

--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/kr/text v0.1.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -35,6 +36,7 @@ require (
 	github.com/opencontainers/image-spec v1.1.0-rc2.0.20221005185240-3a7f492d3f1b // indirect
 	github.com/opencontainers/runc v1.1.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
@@ -45,5 +47,6 @@ require (
 	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gotest.tools/v3 v3.5.0 // indirect
 )
--- internal/app/sqlite.go
+++ internal/app/sqlite.go
@@ -5,58 +5,78 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"path/filepath"
 
-	sqlite3 "github.com/mattn/go-sqlite3"
+	"github.com/bnema/gordon/internal/database/migrate"
+	"github.com/mattn/go-sqlite3"
 )
 
-// Initialize in-memory SQLite database and returns its connection
-func InitDB() (db *sql.DB, err error) {
-	db, err = sql.Open("sqlite3", "file::memory?cache=shared")
-	if err != nil {
-		return nil, fmt.Errorf("failed to open in-memory database: %v", err)
-	}
-	return
-}
-
-// CloseDB closes the database connection and backs up the in-memory database to disk
-func CloseDB(db *sql.DB, a *App) error {
-	if err := BackupDB(db, a); err != nil {
-		return err
-	}
-
-	if err := db.Close(); err != nil {
-		return fmt.Errorf("failed to close database: %v", err)
-	}
-
-	return nil
-}
-
-// LoadDBIntoMemory loads the database from disk into memory
-func LoadDBIntoMemory(a *App) (*sql.DB, error) {
+// InitializeDB initializes the SQLite database. If a database file exists on disk, it loads it into memory.
+// Otherwise, it creates a new in-memory database and bootstraps it.
+func InitializeDB(a *App) (*sql.DB, error) {
 	dbfp := a.GetDBFilePath()
 
-	if _, err := os.Stat(dbfp); err != nil {
-		return nil, nil
+	// Check if the directory exists, if not create it
+	dir := filepath.Dir(a.DBDir)
+	fmt.Println("dir", dir)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create directory: %v", err)
+		}
 	}
 
-	memDB, err := InitDB()
+	// If the database file doesn't exist on disk
+	if _, err := os.Stat(dbfp); os.IsNotExist(err) {
+		memDB, err := initInMemoryDB()
+		if err != nil {
+			return nil, err
+		}
+
+		// Run the bootstrap
+		RunBootstrap(memDB)
+
+		// Explicitly backup the in-memory database to disk
+		err = BackupDB(memDB, a)
+		if err != nil {
+			return nil, fmt.Errorf("failed to backup in-memory database to disk: %v", err)
+		}
+	}
+
+	memDB, err := initInMemoryDB()
 	if err != nil {
 		return nil, err
 	}
-
 	diskDB, err := sql.Open("sqlite3", dbfp)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database file: %v", err)
 	}
+	defer diskDB.Close()
 
 	if err := transferData(diskDB, memDB); err != nil {
 		return nil, err
 	}
-
 	return memDB, nil
 }
 
-// transferData transfers data from srcDB to dstDB
+func initInMemoryDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "file::memory?cache=shared")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open in-memory database: %v", err)
+	}
+	return db, nil
+}
+
+func CloseAndBackupDB(db *sql.DB, a *App) error {
+	if err := BackupDB(db, a); err != nil {
+		return err
+	}
+	if err := db.Close(); err != nil {
+		return fmt.Errorf("failed to close database: %v", err)
+	}
+	return nil
+}
+
 func transferData(srcDB, dstDB *sql.DB) error {
 	srcConn, err := srcDB.Conn(context.Background())
 	if err != nil {
@@ -74,35 +94,50 @@ func transferData(srcDB, dstDB *sql.DB) error {
 		return dstConn.Raw(func(dstRawConn interface{}) error {
 			srcSqlite3Conn := srcRawConn.(*sqlite3.SQLiteConn)
 			dstSqlite3Conn := dstRawConn.(*sqlite3.SQLiteConn)
-
 			_, err := srcSqlite3Conn.Backup("main", dstSqlite3Conn, "main")
 			return err
 		})
 	})
 }
 
-// BackupDB backs up the in-memory database to disk
 func BackupDB(db *sql.DB, a *App) error {
 	dbfp := a.GetDBFilePath()
-
 	conn, err := db.Conn(context.Background())
 	if err != nil {
 		return fmt.Errorf("failed to get underlying database connection: %v", err)
 	}
+	defer conn.Close()
 
 	return conn.Raw(func(sqliteConn interface{}) error {
 		sqlite3Conn := sqliteConn.(*sqlite3.SQLiteConn)
-		backupDB, err := sqlite3Conn.Backup(dbfp, sqlite3Conn, "main")
+		diskDB, err := sql.Open("sqlite3", dbfp)
+		fmt.Println("diskDB", diskDB)
 		if err != nil {
-			return fmt.Errorf("failed to create backup: %v", err)
+			return fmt.Errorf("failed to open destination database file: %v", err)
 		}
+		defer diskDB.Close()
+
+		diskConn, err := diskDB.Conn(context.Background())
+		if err != nil {
+			return fmt.Errorf("failed to get connection from diskDB: %v", err)
+		}
+		defer diskConn.Close()
+
+		var backupDB *sqlite3.SQLiteBackup
+		err = diskConn.Raw(func(diskRawConn interface{}) error {
+			backupDB, err = sqlite3Conn.Backup("main", diskRawConn.(*sqlite3.SQLiteConn), "main")
+			return err
+		})
 		defer backupDB.Close()
 
 		_, err = backupDB.Step(-1)
 		if err != nil {
 			return fmt.Errorf("failed to backup in-memory database: %v", err)
 		}
-
 		return nil
 	})
+}
+
+func RunBootstrap(db *sql.DB) error {
+	return migrate.CreateUserTable(db)
 }
