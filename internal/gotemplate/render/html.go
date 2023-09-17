74c992129fc01ab5ba0828884c62c7e7f11a28d4
bnema <hi@bnema.dev>
2023-09-17 17:02:31 +0200

    Refactoring of app.go for dynamic OAuthCallbackURL based on config.yml settings

--- internal/gotemplate/render/html.go
+++ internal/gotemplate/render/html.go
@@ -17,7 +17,7 @@ type Renderer struct {
 }
 
 // Render function renders the template with the given data
-func (r *Renderer) Render(data interface{}, a *app.App) (string, error) {
+func (r *Renderer) Render(data interface{}, a *app.AppConfig) (string, error) {
 	if r.ParseError != nil {
 		return "", fmt.Errorf("failed to parse template: %w", r.ParseError)
 
