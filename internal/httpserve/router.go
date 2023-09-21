1740d62bccd1c7ce2509d03fc756241bd6f0dc80
bnema <hi@bnema.dev>
2023-09-21 19:52:38 +0200

    Remove button for image-manager added, popup error message customized

--- internal/httpserve/router.go
+++ internal/httpserve/router.go
@@ -70,7 +70,14 @@ func bindHTMXEndpoints(e *echo.Echo, a *app.App) {
 	e.GET("/htmx/image-manager", func(c echo.Context) error {
 		return handler.ImageManagerHandler(c, a)
 	})
-	e.DELETE("/htmx/image-manager/delete/:ShortID", func(c echo.Context) error {
+	e.DELETE("/htmx/image-manager/delete/:ShortImgID", func(c echo.Context) error {
 		return handler.ImageManagerDeleteHandler(c, a)
+	})
+
+	e.GET("/htmx/container-manager", func(c echo.Context) error {
+		return handler.ContainerManagerHandler(c, a)
+	})
+	e.DELETE("/htmx/container-manager/delete/:ShortID", func(c echo.Context) error {
+		return handler.ContainerManagerDeleteHandler(c, a)
 	})
 }
