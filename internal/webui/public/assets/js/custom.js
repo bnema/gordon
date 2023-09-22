478146d00037065a11ddb537ebff7a85b1901691
bnema <hi@bnema.dev>
2023-09-22 10:00:12 +0200

    Better error/succes handling in JS according to what the backend is returning
    - Added "js" to the list of included file extensions in .air.toml.
    - Added new JavaScript files to the HTML templates.
    - Improved error handling in custom.js for htmx requests.

--- internal/webui/public/assets/js/custom.js
+++ internal/webui/public/assets/js/custom.js
@@ -1,15 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.addEventListener('htmx:responseError', function(event) {
-    const errorDetail = event.detail;
-    const errorType = errorDetail && errorDetail.xhr ? errorDetail.xhr.getResponseHeader("X-Error-Type") : 'generic'; // Fallback to 'generic' if not set
-    const errorDiv = document.querySelector(`div[data-error-type="${errorType}"]`);
-    if (errorDiv) {
-      if (errorDetail && errorDetail.xhr && errorDetail.xhr.responseText) {
-        errorDiv.innerHTML = errorDetail.xhr.responseText;
-      } else {
-        errorDiv.textContent = 'An unknown error occurred.';
-      }
-      errorDiv.style.display = 'block'; // Make it visible
+// htmx.logAll();
+document.addEventListener('htmx:afterRequest', function(event) {
+  
+  // Find the error message element
+  var errorMessageElement = document.getElementById('error-message-img');
+  
+  // Find the image div element by ID (you need to get this ID dynamically)
+  var imageDivID = 'img-' + event.detail.elt.id.replace('img-', '');
+  var imageDivElement = document.getElementById(imageDivID);
+  console.log(imageDivElement);
+  
+  // If it's an error, we display and hide the error message
+  if (event.detail.xhr.status !== 200) {
+    if (errorMessageElement && errorMessageElement.textContent.trim() !== "") {
+      // Set a timeout to hide it
+      setTimeout(function() {
+        errorMessageElement.style.display = 'none'; // or set it to empty
+        errorMessageElement.textContent = ''; // clear the text content
+      }, 7000); // 3000 milliseconds = 3 seconds
     }
-  });
+  } else {
+    // If it's a success, we hide the image div
+    if (imageDivElement) {
+      setTimeout(function() {
+        imageDivElement.style.display = 'none'; // Hide the div
+      }, 3000); // 3000 milliseconds = 3 seconds
+    }
+  }
 });
