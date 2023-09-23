01250ef5f28c51ad46b53c7d2fd19a26fa157e63
bnema <hi@bnema.dev>
2023-09-23 11:42:54 +0200

    Add a container images upload feature and auto import into the engine, creating corresponding handlers and routes, and enhancing image import and storage functionalities.

--- pkg/utils/store/storeimages.go
+++ pkg/utils/store/storeimages.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"fmt"
+	"io"
+	"mime/multipart"
 	"os"
 	"path/filepath"
 
@@ -22,20 +24,47 @@ func NewStorageConfig(config *app.AppConfig) *StorageConfig {
 	}
 }
 
-func (sc *StorageConfig) SaveImageToStorage(imagePath string) (string, error) {
-
-	imageId, err := docker.LoadImage(imagePath)
-	if err != nil {
-		return "", fmt.Errorf("failed to load image: %v", err)
+func SaveImageToStorage(config *app.AppConfig, filename string, file multipart.File) (string, error) {
+	// Check if the folder exist if not create it
+	if _, err := os.Stat(config.General.StorageDir); os.IsNotExist(err) {
+		err := os.MkdirAll(config.General.StorageDir, 0755)
+		if err != nil {
+			return "", fmt.Errorf("failed to create storage directory: %v", err)
+		}
 	}
 
-	// Generate the new path for the image in the storage directory
-	newPath := filepath.Join(sc.StorageDir, imageId+".tar")
+	// Define the path where the image will be saved
+	saveInPath := filepath.Join(config.General.StorageDir, filename)
 
-	err = os.Rename(imagePath, newPath)
+	// Create or open a file for appending.
+	outFile, err := os.Create(saveInPath)
 	if err != nil {
-		return "", fmt.Errorf("failed to move image to storage directory: %v", err)
+		return "", fmt.Errorf("failed to open file: %v", err)
+	}
+	defer outFile.Close()
+
+	// Write the uploaded file's content to the outFile
+	_, err = io.Copy(outFile, file)
+	if err != nil {
+		return "", fmt.Errorf("failed to write file: %v", err)
+	}
+
+	// Import the image into Docker
+	imageId, err := docker.ImportImageToEngine(saveInPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to save image to storage directory: %v", err)
 	}
 
 	return imageId, nil
+}
+
+func (sc *StorageConfig) DeleteImageFromStorage(imageId string) error {
+	// Delete the image from the storage directory
+	imagePath := filepath.Join(sc.StorageDir, imageId+".tar")
+	err := os.Remove(imagePath)
+	if err != nil {
+		return fmt.Errorf("failed to delete image from storage directory: %v", err)
+	}
+
+	return nil
 }
