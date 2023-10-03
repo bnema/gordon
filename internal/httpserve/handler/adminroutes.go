ed9f296e91b6b0b5fbe58fac4760fdd1f09fda6b
bnema <hi@bnema.dev>
2023-10-03 20:19:38 +0200

    Fix and refactor code to manage localization more efficiently.     - Added `StringsYML` field in `App` struct to store content of `strings.yml`.     - Modified `NewApp` to read `strings.yml` during app initialization.     - Refactored to use a new `GetLocalizedData` function to get language-specific data.     - Adopted the `GetLocalizedData` function for language data.     - Added dynamic localization using `Lang` map for dashboard title.

--- internal/httpserve/handler/adminroutes.go
+++ internal/httpserve/handler/adminroutes.go
@@ -2,12 +2,10 @@ package handler
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/bnema/gordon/internal/app"
 	"github.com/bnema/gordon/internal/httpserve/middleware"
 	"github.com/bnema/gordon/internal/templates/render"
-	"github.com/bnema/gordon/internal/webui"
 	"github.com/labstack/echo/v4"
 )
 
@@ -16,25 +14,32 @@ var (
 	fragmentsPath = "html/fragments"
 )
 
+func GetLocalizedData(c echo.Context, a *app.App) (map[string]interface{}, error) {
+	lang := c.Get(middleware.LangKey)
+
+	if lang == nil {
+		return nil, fmt.Errorf("LangKey not found in context")
+	}
+
+	yamlData, err := render.GetLocalization(lang.(string), a)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get localization: %w", err)
+	}
+	return map[string]interface{}{
+		"Lang": yamlData,
+	}, nil
+}
+
 // Handle the admin route to display index.gohtml from the templateFS with the data from strings.yml
 func AdminRoute(c echo.Context, a *app.App) error {
-	lang := c.Get(middleware.LangKey).(string)
-	yamlData := webui.StringsYamlData{}
-	err := webui.ReadStringsDataFromYAML(lang, a.TemplateFS, "strings.yml", &yamlData)
+	data, err := GetLocalizedData(c, a)
 	if err != nil {
 		return err
 	}
-
 	// Navigate inside the fs.FS to get the template
 	rendererData, err := render.GetHTMLRenderer(mainPath, "index.gohtml", a.TemplateFS, a, fragmentsPath)
 	if err != nil {
 		return err
-	}
-
-	// Create a data map to pass to the renderer
-	data := map[string]interface{}{
-		"CurrentLang": yamlData.CurrentLang,
-		// "BuildVersion" will be automatically added in the renderer
 	}
 
 	renderedHTML, err := rendererData.Render(data, a)
@@ -46,21 +51,15 @@ func AdminRoute(c echo.Context, a *app.App) error {
 }
 
 func AdminManagerRoute(c echo.Context, a *app.App) error {
-	lang := c.Get(middleware.LangKey).(string)
-	yamlData := webui.StringsYamlData{}
-	err := webui.ReadStringsDataFromYAML(lang, a.TemplateFS, "strings.yml", &yamlData)
+	data, err := GetLocalizedData(c, a)
 	if err != nil {
-		fmt.Printf("Error reading YAML: %v\n", err)
-		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to load language data")
+		return err
 	}
 
 	// Navigate inside the fs.FS to get the template
 	rendererData, err := render.GetHTMLRenderer(mainPath, "manager.gohtml", a.TemplateFS, a, fragmentsPath)
 	if err != nil {
 		return err
-	}
-	data := map[string]interface{}{
-		"CurrentLang": yamlData.CurrentLang,
 	}
 
 	renderedHTML, err := rendererData.Render(data, a)
