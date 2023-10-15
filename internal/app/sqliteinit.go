e228d6a499f2f6428bb5f8b68c39745a25782b16
bnema <hi@bnema.dev>
2023-10-15 22:04:33 +0200

    Refactor to use disk-based SQLite database instead of in-memory
    - Modified main.go to initialize the disk-based SQLite database and removed handling of in-memory database.
    - Removed sqlite_test.go since we no longer have the in-memory database.
    - Simplified sqliteclose.go to only close the disk-based SQLite database.
    - Refactored sqliteinit.go to support disk-based SQLite database initialization.

--- internal/app/sqliteinit.go
+++ internal/app/sqliteinit.go
@@ -1,16 +1,13 @@
 package app
 
 import (
-	"context"
-	"crypto/md5"
 	"database/sql"
-	"encoding/hex"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	pkgsqlite "github.com/bnema/gordon/pkg/sqlite"
-	"github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
@@ -18,42 +15,9 @@ const (
 	DBFilename = "sqlite.db"
 )
 
-func (a *App) GetDiskDBFilePath() string {
+func (a *App) getDiskDBFilePath() string {
 	DiskDBfilepath := filepath.Join(a.DBDir, a.DBFilename)
 	return DiskDBfilepath
-}
-
-// InitializeDB initializes the database
-func InitializeDB(a *App) (*sql.DB, error) {
-	if err := a.ensureDBDir(); err != nil {
-		return nil, fmt.Errorf("failed to ensure DB directory: %w", err)
-	}
-
-	dbPath := a.GetDiskDBFilePath()
-
-	dbExists, err := a.checkDBFile(dbPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to check DB file: %w", err)
-	}
-
-	if !dbExists {
-		if err := bootstrapDB(dbPath, a); err != nil {
-			return nil, fmt.Errorf("failed to bootstrap DB: %w", err)
-		}
-	}
-	memDb, err := loadDBToMemory(dbPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to load DB to memory: %w", err)
-	}
-	// Here we can generate the initial checksum
-	a.InitialChecksum, err = GenerateDBChecksum(memDb, a)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate initial checksum: %w", err)
-	}
-	// Initialize the DB field in the App struct
-	a.DB = memDb
-
-	return memDb, nil
 }
 
 // ensureDBDir ensures that the database directory exists.
@@ -74,6 +38,35 @@ func (a *App) checkDBFile(dbPath string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+// InitializeDB initializes the database
+func InitializeDB(a *App) (*sql.DB, error) {
+	if err := a.ensureDBDir(); err != nil {
+		return nil, fmt.Errorf("failed to ensure DB directory: %w", err)
+	}
+
+	dbPath := a.getDiskDBFilePath()
+
+	dbExists, err := a.checkDBFile(dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check DB file: %w", err)
+	}
+
+	if !dbExists {
+		if err := bootstrapDB(dbPath, a); err != nil {
+			return nil, fmt.Errorf("failed to bootstrap DB: %w", err)
+		}
+	}
+
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open disk DB: %w", err)
+	}
+
+	a.DB = db
+
+	return db, nil
 }
 
 func bootstrapDB(dbPath string, app *App) error {
@@ -99,114 +92,4 @@ func bootstrapDB(dbPath string, app *App) error {
 	}
 
 	return nil
-}
-
-// loadDBToMemory loads the database from disk to memory.
-func loadDBToMemory(dbPath string) (*sql.DB, error) {
-	memDb, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		return nil, err
-	}
-
-	diskDb, err := sql.Open("sqlite3", dbPath)
-	if err != nil {
-		return nil, err
-	}
-	defer diskDb.Close()
-
-	// Get a single connection from each *sql.DB
-	memConn, err := memDb.Conn(context.Background())
-	if err != nil {
-		return nil, err
-	}
-	defer memConn.Close()
-
-	diskConn, err := diskDb.Conn(context.Background())
-	if err != nil {
-		return nil, err
-	}
-	defer diskConn.Close()
-
-	err = memConn.Raw(func(driverConn interface{}) error {
-		memSqliteConn, ok := driverConn.(*sqlite3.SQLiteConn)
-		if !ok {
-			return fmt.Errorf("failed to cast to *sqlite3.SQLiteConn")
-		}
-
-		return diskConn.Raw(func(driverConn interface{}) error {
-			diskSqliteConn, ok := driverConn.(*sqlite3.SQLiteConn)
-			if !ok {
-				return fmt.Errorf("failed to cast to *sqlite3.SQLiteConn")
-			}
-
-			backup, err := memSqliteConn.Backup("main", diskSqliteConn, "main")
-			if err != nil {
-				return err
-			}
-			defer backup.Close()
-
-			done, err := backup.Step(-1)
-			if err != nil {
-				return err
-			}
-			if !done {
-				return fmt.Errorf("backup not fully completed")
-			}
-
-			return backup.Finish()
-		})
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return memDb, nil
-}
-
-// GenerateDBChecksum generates a checksum of the database.
-func GenerateDBChecksum(db *sql.DB, a *App) (string, error) {
-	tables := getTableNames(a)
-	var finalChecksum []byte
-
-	for _, table := range tables {
-		hasher := md5.New() // Create a new hasher for each table
-
-		rows, err := db.Query("SELECT * FROM " + table)
-		if err != nil {
-			return "", err
-		}
-
-		for rows.Next() {
-			cols, err := rows.Columns()
-			if err != nil {
-				return "", err
-			}
-
-			vals := make([]interface{}, len(cols))
-			for i := range cols {
-				vals[i] = new(sql.RawBytes)
-			}
-
-			err = rows.Scan(vals...)
-			if err != nil {
-				return "", err
-			}
-
-			for i := range vals {
-				hasher.Write(*vals[i].(*sql.RawBytes))
-			}
-		}
-
-		if err := rows.Err(); err != nil {
-			rows.Close()
-			return "", err
-		}
-
-		rows.Close()
-
-		finalChecksum = append(finalChecksum, hasher.Sum(nil)...)
-	}
-
-	return hex.EncodeToString(finalChecksum), nil
 }
