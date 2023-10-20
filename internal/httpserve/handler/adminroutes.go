c10a3cb1c6daefbd9e1c3da5e3a06acf35675217
bnema <hi@bnema.dev>
2023-10-20 17:35:15 +0000

    Refactor HTTP Handlers and Utility Functions
    - Generalize admin page rendering into a single `renderAdminPage` function
    - Rewrite the mutex-safe on idMap operations into one function
    - Update status code usages to use `http` package constants
    - Use more descriptive function names and comments for better readability
    - Various comments and code style improvements

--- internal/httpserve/handler/adminroutes.go
+++ internal/httpserve/handler/adminroutes.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"fmt"
 
+	"net/http"
+
 	"github.com/bnema/gordon/internal/app"
 	"github.com/bnema/gordon/internal/httpserve/middleware"
 	"github.com/bnema/gordon/internal/templating/render"
@@ -14,6 +16,7 @@ var (
 	fragmentsPath = "html/fragments"
 )
 
+// GetLocalizedData returns the data for the localization
 func GetLocalizedData(c echo.Context, a *app.App) (map[string]interface{}, error) {
 	lang := c.Get(middleware.LangKey)
 
@@ -30,14 +33,14 @@ func GetLocalizedData(c echo.Context, a *app.App) (map[string]interface{}, error
 	}, nil
 }
 
-// Handle the admin route to display index.gohtml from the templateFS with the data from strings.yml
-func AdminRoute(c echo.Context, a *app.App) error {
+// renderAdminPage is a generalized function to render admin pages
+func renderAdminPage(c echo.Context, a *app.App, templateName string) error {
 	data, err := GetLocalizedData(c, a)
 	if err != nil {
 		return err
 	}
-	// Navigate inside the fs.FS to get the template
-	rendererData, err := render.GetHTMLRenderer(mainPath, "index.gohtml", a.TemplateFS, a, fragmentsPath)
+
+	rendererData, err := render.GetHTMLRenderer(mainPath, templateName, a.TemplateFS, a, fragmentsPath)
 	if err != nil {
 		return err
 	}
@@ -47,25 +50,15 @@ func AdminRoute(c echo.Context, a *app.App) error {
 		return err
 	}
 
-	return c.HTML(200, renderedHTML)
+	return c.HTML(http.StatusOK, renderedHTML)
 }
 
+// AdminRoute is the route for the admin panel
+func AdminRoute(c echo.Context, a *app.App) error {
+	return renderAdminPage(c, a, "index.gohtml")
+}
+
+// AdminManagerRoute is the route of the manager page
 func AdminManagerRoute(c echo.Context, a *app.App) error {
-	data, err := GetLocalizedData(c, a)
-	if err != nil {
-		return err
-	}
-
-	// Navigate inside the fs.FS to get the template
-	rendererData, err := render.GetHTMLRenderer(mainPath, "manager.gohtml", a.TemplateFS, a, fragmentsPath)
-	if err != nil {
-		return err
-	}
-
-	renderedHTML, err := rendererData.Render(data, a)
-	if err != nil {
-		return err
-	}
-
-	return c.HTML(200, renderedHTML)
+	return renderAdminPage(c, a, "manager.gohtml")
 }
