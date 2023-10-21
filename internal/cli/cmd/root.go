4191a1341773912f03871da1c3a4db4cf8a4a333
bnema <hi@bnema.dev>
2023-10-21 14:34:16 +0000

    Add basic bubbletea example

---
+++ internal/cli/cmd/root.go
@@ -0,0 +1,8 @@
+// the root command is the entrypoint for the gordon cli (default: client)
+package cmd
+
+// if no command selected print all commands
+// available commands for now are:
+// - push
+// - serve
+// - version
---
+++ internal/cli/cmd/serve.go
@@ -0,0 +1,3 @@
+// the serve command is used to start the gordon server
+// Path: internal/cli/cmd/serve.go
+package cmd
