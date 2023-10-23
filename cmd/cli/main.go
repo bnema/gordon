41f614673900f68d2e065f53e0b77112a2085805
bnema <hi@bnema.dev>
2023-10-23 20:58:38 +0200

    Refactor NewClientApp and Configuration Logic
    - Add error handling in NewClientApp
    - Simplify configuration loading and saving
    - Remove redundant config handler in CLI

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bnema/gordon/internal/app"
 	"github.com/bnema/gordon/internal/cli"
 )
 
 func main() {
-	a := app.NewClientApp()
+	a, err := app.NewClientApp()
+	if err != nil {
+		fmt.Println("Error initializing app:", err)
+	}
 	cli.Execute(a)
 }
