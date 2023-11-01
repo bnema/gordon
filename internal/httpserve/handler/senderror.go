f63f3f688fa8e5366da8db196d097d9bd48698af
bnema <hi@bnema.dev>
2023-11-01 09:37:14 +0100

    Complete Push command feature
    - Implement backend flag parsing logic to deploy image to a specific domain
    - Introduce `ImageID` field in `PushPayload` struct
    - Improve error handling in `PostPush` method with `sendJsonError`
    - Add response verification and domain availability check in `push.go`
    - Wait & check url availability in final step of push command

--- internal/httpserve/handler/senderror.go
+++ internal/httpserve/handler/senderror.go
@@ -15,3 +15,7 @@ func sendError(c echo.Context, err error) error {
 	}
 	return c.HTML(http.StatusInternalServerError, sanitizedHTML)
 }
+
+func sendJsonError(c echo.Context, err error) error {
+	return c.JSON(http.StatusInternalServerError, err.Error())
+}
