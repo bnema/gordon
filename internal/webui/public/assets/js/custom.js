fbb74da15b7818204b6d342664779548b43680b2
bnema <hi@bnema.dev>
2023-11-07 10:47:26 +0100

    Fix: webui notify update, state not updating  when new release available

--- internal/webui/public/assets/js/custom.js
+++ internal/webui/public/assets/js/custom.js
@@ -1,21 +1,16 @@
 function initializeVersionCheck() {
+  // Dummy data for the remote version (as if fetched from a server)
   const updateStrTemplate = "New version %VERSION% available, consider pulling the latest image";
 
-  const checkVersion = (versionData) => {
-    const currentVersion = document.getElementById("actual-version").textContent.trim();
+  const checkVersion = (versionData, currentVersion) => {
     const fetchedVersionNumber = versionData?.amd64?.name.match(/\d+\.\d+\.\d+/)?.[0];
-
-    if (!fetchedVersionNumber) {
-      console.log("Dev mode detected, skipping version check.");
-      return;
-    }
 
     if (currentVersion !== fetchedVersionNumber) {
       const updateStr = updateStrTemplate.replace("%VERSION%", fetchedVersionNumber);
       console.log(updateStr);
       const updateElement = document.getElementById("update-available");
       updateElement.title = updateStr;
-      updateElement.removeAttribute("hidden");
+      updateElement.toggleAttribute("hidden", false);
       updateElement.classList.remove("hidden"); // Remove the Tailwind `hidden` class
       updateElement.classList.add("block"); // Add the Tailwind `block` class
     } else {
@@ -23,20 +18,25 @@ function initializeVersionCheck() {
     }
   };
 
-  const fetchVersionInfo = () => {
+  const fetchVersionInfo = (currentVersion) => {
     fetch("https://gordon-proxy.bnema.dev/version")
       .then((response) => response.json())
-      .then(checkVersion)
+      .then((versionData) => checkVersion(versionData, currentVersion))
       .catch((error) => console.error("Error fetching version:", error));
   };
 
   const init = () => {
-    const currentVersion = document.getElementById("actual-version").textContent.trim();
+    const currentVersionElement = document.getElementById("actual-version");
+    if (!currentVersionElement) {
+      console.log("Error: #actual-version element not found.");
+      return;
+    }
+    const currentVersion = currentVersionElement.textContent.trim();
     if (!currentVersion) {
       console.log("Dev mode detected, skipping version check.");
       return;
     }
-    fetchVersionInfo();
+    fetchVersionInfo(currentVersion);
   };
 
   init();
