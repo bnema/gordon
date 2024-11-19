948a1413ccdb4838e11ede1d37e6fecbb70a2347
Brice <hi@bamen.dev>
2024-11-19 21:11:54 +0100

    refactor: use version infos via ldflags and simplify version checking logic

--- internal/cli/cmd/version.go
+++ internal/cli/cmd/version.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/bnema/gordon/internal/cli"
+	"github.com/bnema/gordon/internal/common"
 	"github.com/spf13/cobra"
 )
 
@@ -12,11 +13,22 @@ func NewVersionCommand(a *cli.App) *cobra.Command {
 		Use:   "version",
 		Short: "Display the version of Gordon",
 		Run: func(cmd *cobra.Command, args []string) {
-			version := a.Config.Build.BuildVersion
-			if version == "" {
-				fmt.Println("Version: devel")
-			} else {
-				fmt.Printf("Version: %s\n", version)
+			info := common.GetVersionInfo(
+				a.Config.Build.BuildVersion,
+				a.Config.Build.BuildCommit,
+				a.Config.Build.BuildDate,
+			)
+			fmt.Println(info.String())
+
+			// Check for updates
+			hasUpdate, latestVersion, err := common.CheckForNewVersion(info.Version)
+			if err != nil {
+				fmt.Printf("Error checking for updates: %v\n", err)
+				return
+			}
+
+			if hasUpdate {
+				fmt.Printf("\nNew version %s available! Visit: https://github.com/bnema/gordon/releases/latest\n", latestVersion)
 			}
 		},
 	}
