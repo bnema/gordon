4bf9f062f38862e7ae94bb4606f49f9978602548
Brice <hi@bamen.dev>
2024-11-20 10:09:54 +0100

    refactor: use version infos via ldflags and simplify version checking logic

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"github.com/bnema/gordon/cmd"
+	"github.com/charmbracelet/log"
+)
+
+var (
+	version string
+	commit  string
+	date    string
 )
 
 func main() {
-	cmd.ExecuteCLI()
+	log.Info("Starting Gordon", "version", version, "commit", commit, "date", date)
+	cmd.ExecuteCLI(version, commit, date)
 }
