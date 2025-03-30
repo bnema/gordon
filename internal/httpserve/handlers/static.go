d255313bcc5852eba9a6861775138489ec449384
bamen <hi@bamen.dev>
2025-03-30 15:01:22 +0200

    feat: Enhance static file serving and image management notifications
    - Implemented a new static file serving mechanism under the `/assets/*` path with cache control based on the environment.
    - Refactored the image management UI to include a notification area for displaying messages related to image pruning operations.
    - Updated the `ImageManagerPrune` handler to run pruning in the background and notify the client of the operation's status.
    - Added event handling in the frontend to display success or error messages based on the pruning result.
    - Removed deprecated static route handling for improved clarity and maintainability.

--- internal/httpserve/handlers/static.go
+++ internal/httpserve/handlers/static.go
@@ -3,15 +3,16 @@ package handlers
 import (
 	"io/fs"
 	"net/http"
-	"os"
-
-	"github.com/bnema/gordon/internal/server"
-	"github.com/labstack/echo/v4"
 )
 
 // NoDirFileSys restricts directory listing
 type NoDirFileSys struct {
 	fs http.FileSystem
+}
+
+// NewNoDirFileSys creates a new NoDirFileSys
+func NewNoDirFileSys(fs http.FileSystem) NoDirFileSys {
+	return NoDirFileSys{fs: fs}
 }
 
 func (nfs NoDirFileSys) Open(name string) (http.File, error) {
@@ -33,23 +34,4 @@ func (f NoDirFile) Readdir(count int) ([]fs.FileInfo, error) {
 	return nil, nil
 }
 
-// StaticRoute serves static files from the embedded filesystem
-func StaticRoute(c echo.Context, a *server.App) error {
-	// Set the cache-control header if we are not in dev mode
-	if os.Getenv("RUN_ENV") != "dev" {
-		c.Response().Header().Set("Cache-Control", "public, max-age=86400")
-	} else {
-		c.Response().Header().Set("Cache-Control", "no-cache")
-	}
-
-	// Convert fs.FS to http.FileSystem
-	httpFS := http.FS(a.PublicFS)
-
-	// Use custom file system wrapper to disable directory listing
-	noDirFS := NoDirFileSys{httpFS}
-
-	// Serve the file from the custom filesystem
-	publicFS := http.FileServer(noDirFS)
-	publicFS.ServeHTTP(c.Response(), c.Request())
-	return nil
-}
+// StaticRoute serves static files from the embedded filesystem - REMOVED
